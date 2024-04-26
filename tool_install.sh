5751bc8c88af272c343a3dd461cad881036b6c06
mkshahi <mkshahi2004@gmail.com>
2024-04-26 20:39:25 +0530

    Update tool_install.sh

--- tool_install.sh
+++ tool_install.sh
@@ -1,8 +1,12 @@
-sudo apt update sudo apt upgrade
+sudo apt update 
+sudo apt upgrade
 sudo apt install openjdk-11-jdk
 wget -q -O - https://pkg.jenkins.io/debian/jenkins.io.key | sudo apt-key add - 
 sudo sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 5BA31D57EF5975CA
-sudo apt update sudo apt install jenkins
+sudo apt update 
+sudo apt install jenkins
 sudo systemctl start jenkins
 sudo systemctl enable jenkins
+sudo cat /var/lib/jenkins/secrets/initialAdminPassword
+
