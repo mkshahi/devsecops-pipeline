6fa46d550d9e64a93f7e6f765ce4cfdef217743c
mkshahi <mkshahi2004@gmail.com>
2024-04-27 12:36:52 +0530

    add trivy

--- tool_install.sh
+++ tool_install.sh
@@ -13,3 +13,11 @@ sudo cat /var/lib/jenkins/secrets/initialAdminPassword
 sudo apt-get install docker.io -y
 sudo usermod -a -G docker $(whoami)
 
+docker run -itd --name sonar -p 9000:9000 sonarqube:lts-community
+
+sudo apt-get install wget apt-transport-https gnupg lsb-release
+wget -qO - https://aquasecurity.github.io/trivy-repo/deb/public.key | gpg --dearmor | sudo tee /usr/share/keyrings/trivy.gpg > /dev/null
+echo "deb [signed-by=/usr/share/keyrings/trivy.gpg] https://aquasecurity.github.io/trivy-repo/deb $(lsb_release -sc) main" | sudo tee -a /etc/apt/sources.list.d/trivy.list
+sudo apt-get update
+sudo apt-get install trivy
+
